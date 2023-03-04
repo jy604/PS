106cb16af659917e4ae1eaaed3c427a4243de6a0
jy604 <126448821+jy604@users.noreply.github.com>
2023-03-04 13:59:26 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 9680 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -1,12 +1,20 @@
 import sys
 
 n = int(sys.stdin.readline())
-num = [0] * 10001
+cnt = [0] * 10001 #작업 배열
+
+#메모리 초과 > 두 식 합치기
+# for _ in range(n): #배열에 입력된 값 넣어 raw 배열 만들기
+#     array.append(int(sys.stdin.readline()))
+#
+# for i in range(n):
+#     cnt[array[i]] += 1
 
 for _ in range(n):
-    num[int(sys.stdin.readline())] += 1
+    cnt[int(sys.stdin.readline())] += 1
 
+#런타임에러 > cnt 전체 안돌도록 조건 달아줘야함
 for i in range(10001):
-    if num[i] != 0:
-        for j in range(num[i]):
+    if cnt[i] != 0:
+        for j in range(cnt[i]):
             print(i)
