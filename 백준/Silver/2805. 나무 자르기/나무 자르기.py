5cfc003e5a112e963eb5dc654a37dd2595aaf6d3
jy604 <126448821+jy604@users.noreply.github.com>
2023-03-12 14:43:57 +0900

    [Silver II] Title: 나무 자르기, Time: 4704 ms, Memory: 143400 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -1,23 +1,18 @@
 import sys
-n, m = map(int, sys.stdin.readline().split())
-tree = list(map(int, sys.stdin.readline().split()))
+N, M = map(int,sys.stdin.readline().split())
+trees = list(map(int,sys.stdin.readline().split()))
 ans = 0
-
-tree.sort()
-
-start = 0
-end = max(tree)
-
-while start <= end:
+left, right = 0, max(trees)
+trees.sort()
+while left <= right:
     total = 0
-    mid = (start + end) // 2
-    for x in tree:
-        if mid < x:
-            total += (x - mid) # 자른 길이의 합
-    if total < m: #나무가 모자랄 경우
-        end = mid - 1 #왼쪽 탐색을 하기 위해 end를 줄임
+    mid = (left+right)//2
+    for tree in trees:
+        if tree > mid:
+            total += (tree-mid)
+    if total < M:
+        right = mid -1
     else:
         ans = mid
-        start = mid + 1
-
+        left = mid +1
 print(ans)
