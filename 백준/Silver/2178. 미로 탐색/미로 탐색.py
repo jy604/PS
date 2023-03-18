15da73d72d9df998ec27dead97ee3744571b005f
jy604 <126448821+jy604@users.noreply.github.com>
2023-03-18 15:49:32 +0900

    [Silver I] Title: 미로 탐색, Time: 80 ms, Memory: 34140 KB -BaekjoonHub

---
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -0,0 +1,36 @@
+import sys
+from collections import deque
+
+n, m = map(int, input().split())
+graph = []
+for i in range(n):
+    graph.append(list(map(int, input())))
+
+# 방향 표시 상, 하, 좌, 우
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+
+def bfs(x, y):
+    q = deque()
+    q.append((x, y))
+
+    while q:
+        x, y = q.popleft()
+        # 4 방향으로 위치 확인
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            # 미로에 벗어난 경우 처리하는 부분
+            if nx < 0 or ny < 0 or nx >= n or ny >= m:
+                continue
+            #벽인 부분
+            if graph[nx][ny] == 0:
+                continue
+            if graph[nx][ny] == 1: # 방문했을 경우
+                graph[nx][ny] = graph[x][y] + 1
+                q.append((nx, ny))
+    return graph[n - 1][m - 1]
+
+
+print(bfs(0,0))
