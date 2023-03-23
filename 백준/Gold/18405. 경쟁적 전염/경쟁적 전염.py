d4e0bbaa2eeccbcdce9c8f5f84924bde413bf57e
jy604 <126448821+jy604@users.noreply.github.com>
2023-03-23 11:55:17 +0900

    [Gold V] Title: 경쟁적 전염, Time: 128 ms, Memory: 34184 KB -BaekjoonHub

---
+++ 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.py
@@ -0,0 +1,42 @@
+import sys
+from collections import deque
+# input = sys.stdin.readline
+
+n, k = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(n)]
+vi = []
+for i in range(n):
+    for j in range(n):
+        if graph[i][j] != 0:
+            vi.append((graph[i][j], i, j))
+s, x, y = map(int, input().split())
+# print(graph)
+# print(vi)
+# visited = [[0] * n for _ in range(n)]
+
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
+
+
+def bfs(s, x, y):
+    q = deque(vi)
+    time = 0
+    while q:
+        if time == s:
+            break
+        for _ in range(len(q)):
+            p, a, b = q.popleft()
+            for i in range(4):
+                nx = a + dx[i]
+                ny = b + dy[i]
+                if 0 <= nx < n and 0 <= ny < n:
+                    if graph[nx][ny] == 0:
+                        graph[nx][ny] = graph[a][b]
+                        q.append((graph[nx][ny], nx, ny))
+        time += 1
+    return graph[x - 1][y - 1]
+
+
+vi.sort()
+print(bfs(s, x, y))
+
