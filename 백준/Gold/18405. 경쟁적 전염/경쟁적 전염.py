3b02c076c3f0ec1836acccd8b805460a01164705
jy604 <126448821+jy604@users.noreply.github.com>
2023-03-23 11:58:42 +0900

    [Gold V] Title: 경쟁적 전염, Time: 108 ms, Memory: 34192 KB -BaekjoonHub

--- 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.py
+++ 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.py
@@ -1,6 +1,6 @@
 import sys
 from collections import deque
-# input = sys.stdin.readline
+input = sys.stdin.readline
 
 n, k = map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(n)]
