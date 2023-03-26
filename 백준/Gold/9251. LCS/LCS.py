baf86c3911f2b23132703acde9b11428ffc2c2d5
jy604 <126448821+jy604@users.noreply.github.com>
2023-03-26 20:06:29 +0900

    [Gold V] Title: LCS, Time: 536 ms, Memory: 55712 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.py
@@ -0,0 +1,18 @@
+#LCS
+a = input().strip()
+b = input().strip()
+len_a = len(a)
+len_b = len(b)
+
+dp = [[0] * (len(b) + 1) for _ in range(len_a + 1)]
+
+# print(*dp)
+
+for i in range(1, len(a) + 1):
+    for j in range(1, len(b) + 1):
+        if a[i - 1] == b[j - 1]:
+            dp[i][j] = dp[i - 1][j - 1] + 1
+        else:
+            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+
+print(dp[-1][-1])
