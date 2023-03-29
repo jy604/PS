ef6d8627988ced34a91d74c431339b24dad5556a
jy604 <126448821+jy604@users.noreply.github.com>
2023-03-29 16:42:42 +0900

    [Silver II] Title: 외판원 순회 2, Time: 140 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
@@ -0,0 +1,31 @@
+import sys
+
+
+def dfs(start, now, value, cnt):
+    global ans
+    if cnt == n:
+        if a[now][start]:
+            value += a[now][start]
+            if ans > value:
+                ans = value
+        return
+
+    if value > ans:
+        return
+
+    for i in range(n):
+        if not visited[i] and a[now][i]:
+            visited[i] = 1
+            dfs(start, i, value + a[now][i], cnt+1)
+            visited[i] = 0
+
+
+n = int(input())
+a = [list(map(int, input().split())) for _ in range(n)]
+visited = [0] * n
+ans = sys.maxsize
+for i in range(n):
+    visited[i] = 1
+    dfs(i, i, 0, 1)
+    visited[i] = 0
+print(ans)
