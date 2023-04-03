9bf6774f83c3bd7920fa9e37b2e700ed819dc38c
jy604 <126448821+jy604@users.noreply.github.com>
2023-04-03 09:27:51 +0900

    [level 2] Title: H-Index, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42747. H－Index/H－Index.py
@@ -0,0 +1,14 @@
+def solution(citations):
+    citations.sort() #0, 1, 3, 5, 6
+    cnt = len(citations) # 전체 논문 수
+    answer = 0
+    for i in range(cnt):
+        if citations[i] >= cnt - i:
+            # if cnt - i != citations[i]:
+            #     return cnt
+            return cnt - i
+
+    
+    return answer
+
+# 반례 : h값이 배열에 들어있지 않는 경우
