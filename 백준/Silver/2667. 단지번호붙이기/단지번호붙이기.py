faf3920301daa1113f13d053f332df954637d5dd
jy604 <126448821+jy604@users.noreply.github.com>
2024-06-25 17:29:59 +0900

    [Silver I] Title: 단지번호붙이기, Time: 44 ms, Memory: 31180 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,46 +1,34 @@
-# 단지번호 붙이기
-
-import sys
-from collections import deque
-input = sys.stdin.readline
-
-
-def bfs(g, x, y):
-    q = deque()
-    q.append((x, y))
-    cur_count = 0
-    if g[x][y] == '1':
-        cur_count = 1
-    visited[x][y] = 1
-    while q:
-        x, y = q.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if nx in range(n) and ny in range(n):
-                if g[nx][ny] == '1' and not visited[nx][ny]:
-                    visited[nx][ny] = 1
-                # g[nx][ny] = 0 # 다시 방문 못하게 0으로 바꿈
-                # g[nx][ny] = g[x][y] + 1
-                    cur_count += 1
-                    q.append((nx, ny))
-    return cur_count
-
-
 n = int(input())
-g = []
-for _ in range(n):
-    g.append(input().rstrip())
-dx = [1, -1, 0, 0]
-dy = [0, 0, 1, -1]
-visited = [[0] * n for _ in range(n)]
+graph = [list(map(int, input())) for _ in range(n)]
+
+def dfs(x,y):
+    global count
+    # 범위를 벗어난 경우
+    if x <= -1 or x >= n or y <= -1 or y >= n:
+        return False
+    # 현재 위치가 집이 아닌 경우
+    if graph[x][y] == 0:
+        return False
+    # 현재 위치 방문 처리(0으로 변경)!!!
+    graph[x][y] = 0
+    count += 1
+
+    dfs(x - 1, y)
+    dfs(x + 1, y)
+    dfs(x, y - 1)
+    dfs(x, y + 1)
+
+
 result = []
+
 for i in range(n):
     for j in range(n):
-        if g[i][j] == '1' and not visited[i][j]:
-            result.append(bfs(g, i, j))
+        if graph[i][j] == 1:
+            count = 0
+            dfs(i, j)
+            result.append(count)
 
 result.sort()
 print(len(result))
-for i in result:
-    print(i)
+for s in result:
+    print(s)
