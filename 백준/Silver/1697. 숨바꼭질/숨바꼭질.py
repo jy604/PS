490c1fbb174754e55d8551a92a775813313969a0
jy604 <126448821+jy604@users.noreply.github.com>
2024-07-02 02:30:56 +0900

    [Silver I] Title: 숨바꼭질, Time: 88 ms, Memory: 35660 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -1,23 +1,22 @@
+# 1697
 # 숨바꼭질
-# 수빈, 동생 위치
 from collections import deque
-
-
+n, k = map(int, input().split())
+max = 100001
+# dx = [-1, 1, *2] int형이어야함
+result = [0] * (max + 1)
 def bfs(x):
     q = deque([x])
 
     while q:
         x = q.popleft()
         if x == k:
-            return dist[x]
-        for nx in (x - 1, x + 1, 2 * x):
-            if 0 <= nx <= max and not dist[nx]:
-                dist[nx] = dist[x] + 1
+            return result[x]
+        # for i in range(3):
+        #     nx = x + dx[i]
+        for nx in (x - 1, x + 1, x * 2):
+            if 0 <= nx <= max and result[nx] == 0:
+                result[nx] = result[x] + 1
                 q.append(nx)
-
-
-n, k = map(int, input().split())
-max = 100001
-dist = [0] * (max + 1)
 
 print(bfs(n))
