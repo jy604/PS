ac0d06b57c40055456dcc632e3245f9f255545c2
jy604 <126448821+jy604@users.noreply.github.com>
2024-07-21 14:34:39 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 68 ms, Memory: 34072 KB -BaekjoonHub

---
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.py
@@ -0,0 +1,30 @@
+# 1389 케빈 베이컨의 6단계 법칙
+from collections import deque
+n, m = map(int, input().split())
+g = [[] for _ in range(n + 1)]
+for i in range(m):
+    a, b = map(int, input().split())
+    g[a].append(b)
+    g[b].append(a)
+
+def bfs(start):
+    q = deque()
+    q.append(start)
+    # 각 노드별 최소경로이기때문에 방문할때마다 초기화해야함
+    visited = [-1] * (n + 1)
+    visited[start] = 0
+
+    while q:
+        start = q.popleft()
+
+        for i in g[start]:
+            if visited[i] == -1:
+                visited[i] = visited[start] + 1
+                q.append(i)
+    result = sum(visited[1:])
+    return result
+
+res = []
+for i in range(1, n + 1):
+    res.append(bfs(i))
+print(res.index(min(res)) + 1)
