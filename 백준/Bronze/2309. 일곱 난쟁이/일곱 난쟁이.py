ae649772c3901f1a3af37777a3521367256deb59
jy604 <126448821+jy604@users.noreply.github.com>
2025-02-15 04:32:01 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -1,12 +1,21 @@
-from itertools import combinations
-k = sorted([int(input()) for _ in range(9)])
+# 조합 안 쓰고 이중반복문으로 찾기
+k = [int(input()) for _ in range(9)]
+total_height = sum(k) # 난쟁이 전체 키의 합
 
-com_list =list(combinations(k, 7))
+# 가짜 두 명 고르기
+# 이중 반복문 사용
 
-for i in com_list:
-    ans = sum(i)
-    if ans == 100:
-        # print(i) 튜플 형태로 출력
-        for j in i:
-            print(j)
+f1, f2 = 0, 0
+for i in range(8):
+    for j in range(i + 1, 9):
+        if k[i] + k[j] == total_height - 100:
+            f1, f2 = k[i], k[j]
+            break
+    if f1 != 0:
         break
+
+k.remove(f1)
+k.remove(f2)
+
+for i in sorted(k):
+    print(i)
