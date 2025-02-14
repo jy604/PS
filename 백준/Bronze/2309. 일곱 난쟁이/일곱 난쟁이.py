af943f91208b6fc0dcc8f7649e2d5e1db4e52eb8
jy604 <126448821+jy604@users.noreply.github.com>
2025-02-15 02:19:02 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -1,9 +1,12 @@
-import itertools
-#조합 라이브러리 이용
-h = [int(input()) for _ in range(9)] 
+from itertools import combinations
+k = sorted([int(input()) for _ in range(9)])
 
-for i in itertools.combinations(h, 7):
-    if sum(i) == 100:
-        for j in sorted(i): #오름차순 정렬
+com_list =list(combinations(k, 7))
+
+for i in com_list:
+    ans = sum(i)
+    if ans == 100:
+        # print(i) 튜플 형태로 출력
+        for j in i:
             print(j)
         break
